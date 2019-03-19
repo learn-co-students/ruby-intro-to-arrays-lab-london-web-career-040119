c08abcb5c4f9a8922d25493bdf50411c97d849c3
github username <github email address>
2019-03-19 16:23:38 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,38 @@
+def instantiate_new_array
+    my_new_array = []
+    my_new_array
+end
+
+def array_with_two_elements
+    my_new_array = [1, 2]
+    my_new_array
+end
+
+taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+
+def first_element(array)
+    my_first_element = array[0]
+    my_first_element
+end
+
+def third_element(array)
+    my_third_element = array[2]
+    my_third_element
+end
+
+def last_element(array)
+    my_last_element = array[-1]
+    my_last_element
+end
+
+def first_element_with_array_methods(array)
+    array.first
+end
+
+def last_element_with_array_methods(array)
+    array.last
+end
+
+def length_of_array(array)
+    array.length
+end
